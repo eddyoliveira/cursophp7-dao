4f7abca3f328f6e71b01a98b66cc2752c022b3c6
eddyoliveira <eddyoliveirahd@gmail.com>
2018-04-28 10:27:33 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
 
 require_once("config.php");
+//Carrega um usuário
+// $eddy = new Usuario();
+// $eddy->loadByid(6);
+// echo $eddy;
 
-$slq = new Sql();
+//Carrega uma lista de usuáradios
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios"); 
+// Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("ad");
+// echo json_encode($search);
 
-echo json_encode($usuarios);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("eddy","123456");
+
+echo $usuario;
+
+
+
+/*$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+echo json_encode($usuarios);*/
  ?>
